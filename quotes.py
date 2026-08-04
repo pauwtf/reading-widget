179d4298fec46af222cc1ecf133d9172267d136a
Paula Gutierrez <109836054+pauwtf@users.noreply.github.com>
2026-08-03 21:30:48 -0500

    Actualizar quotes.py

--- quotes.py
+++ quotes.py
@@ -1,33 +1,7 @@
 import random
-import requests
-
-from notion import HEADERS, QUOTES_DATABASE_URL
-
-
-def get_quotes_for_book(book_id):
-
-    payload = {
-        "filter": {
-            "property": "Libro",
-            "relation": {
-                "contains": book_id
-            }
-        }
-    }
-
-    response = requests.post(
-        QUOTES_DATABASE_URL,
-        headers=HEADERS,
-        json=payload
-    )
-
-    response.raise_for_status()
-
-    return response.json()["results"]
 
 
 def get_text(prop):
-
     title = prop["title"]
 
     if not title:
@@ -40,12 +14,10 @@ def get_text(prop):
 
 
 def get_number(prop):
-
-    return prop["number"] or 0
+    return prop["number"] if prop["number"] is not None else 0
 
 
 def get_checkbox(prop):
-
     return prop["checkbox"]
 
 
@@ -57,6 +29,14 @@ def get_random_quote(quotes):
     quote = random.choice(quotes)
 
     properties = quote["properties"]
+
+    print("\n========== PROPIEDADES FRASE ==========")
+
+    for key, value in properties.items():
+        print(f"{key:<20} -> {value['type']}")
+
+    print("=======================================\n")
+
 
     return {
         "text": get_text(properties["Frase"]),
