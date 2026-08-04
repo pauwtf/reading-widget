6f54a4b2a750a7f22cb20b48c2a795d08d9ab921
Paula Gutierrez <109836054+pauwtf@users.noreply.github.com>
2026-08-03 20:39:18 -0500

    Actualizar books.py

--- books.py
+++ books.py
@@ -1,32 +1,5 @@
 from datetime import datetime, timezone
-
-
-def get_title(prop):
-    if not prop["title"]:
-        return ""
-    return "".join(item["plain_text"] for item in prop["title"])
-
-
-def get_formula_text(prop):
-    formula = prop["formula"]
-
-    if formula["type"] == "string":
-        return formula["string"] or ""
-
-    return ""
-
-
-def get_number(prop):
-    return prop["number"] if prop["number"] is not None else 0
-
-
-def get_progress(prop):
-    formula = prop["formula"]
-
-    if formula["type"] == "number" and formula["number"] is not None:
-        return round(formula["number"] * 100)
-
-    return 0
+from utils import get_title, get_formula_text, get_number, get_progress
 
 
 def build_current_book(book):
