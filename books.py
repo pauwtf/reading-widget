6bf45358cfa65dc0b2c160264b2f097b878ff5eb
Paula Gutierrez <109836054+pauwtf@users.noreply.github.com>
2026-08-05 11:31:50 -0500

    Actualizar books.py

--- books.py
+++ books.py
@@ -126,6 +126,8 @@ def build_current_book(book, book_id):
 
         "displayAuthor":
             build_author_display(author),
+						"debug":
+ 							   "VERSION 1.2",
 
         "genre": get_formula_text(
             book["Genero Nombre"]
