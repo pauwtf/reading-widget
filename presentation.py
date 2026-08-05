a7a2faa1e213fbc13f18bf546951029e0e57708f
Paula Gutierrez <109836054+pauwtf@users.noreply.github.com>
2026-08-05 13:29:05 -0500

    Actualizar presentation.py

--- presentation.py
+++ presentation.py
@@ -3,6 +3,9 @@ from display import (
     get_title_font_size,
     build_author_display,
     build_display_text,
+    build_quote_display,
+    get_quote_font_size,
+    build_page_display,
 )
 
 
@@ -11,6 +14,10 @@ def build_book_display(
     author,
     genre,
 ):
+    """
+    Construye todos los campos de presentación
+    relacionados con un libro.
+    """
 
     title_display = build_title_display(
         title
@@ -22,14 +29,49 @@ def build_book_display(
             title_display,
 
         "displayAuthor":
-            build_author_display(author),
+            build_author_display(
+                author
+            ),
 
         "displayGenre":
-            build_display_text(genre),
+            build_display_text(
+                genre
+            ),
 
         "titleFontSize":
             get_title_font_size(
                 title_display
+            )
+
+    }
+
+
+def build_quote_presentation(
+    text,
+    page,
+):
+    """
+    Construye todos los campos de presentación
+    relacionados con una cita.
+    """
+
+    quote_display = build_quote_display(
+        text
+    )
+
+    return {
+
+        "display":
+            quote_display,
+
+        "fontSize":
+            get_quote_font_size(
+                quote_display
+            ),
+
+        "pageDisplay":
+            build_page_display(
+                page
             )
 
     }
